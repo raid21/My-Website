0893e16330db01dcb6b285f0e636d390579307c4
raid21 <raidski20@gmail.com>
2020-03-18 17:59:34 +0100

    first commit

---
+++ js/bs4tuto.js
@@ -0,0 +1,25 @@
+$(function (){
+    'use strict';
+    // set the height of header's slider
+    var windowHeight = $(window).height(),
+        navH  = $('.navbar').innerHeight();
+    $('.slider, .carousel-item').height(windowHeight - navH);
+
+    // start of scroll to top button
+    var btn = $('#button');
+    $(window).scroll(function() {
+    if ($(window).scrollTop() > 200) {
+        btn.addClass('show');
+    } 
+    else 
+    {
+        btn.removeClass('show');
+    }
+    });
+
+    btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, '300');
+    });
+// end of scroll to top button
+})
