42a3d0f6d75aef1f36204f47ea7ace18538f6c71
raid21 <raidski20@gmail.com>
2020-03-20 12:16:49 +0100

    third commit (adding modal box to portfolio section).

--- js/bs4tuto.js
+++ js/bs4tuto.js
@@ -30,3 +30,47 @@ $(function (){
     }, 1000);
     });
 })
+
+// start of portfolio images modal
+var ids_tab = ["img/port/boat.png","img/port/uipage.png",
+               "img/port/login.png","img/port/regis.png"];
+
+function openModel (id) {
+
+    switch(id) {
+        case 'port1':
+            create_ele(ids_tab[0]);
+        break;
+
+        case 'port2':
+            create_ele(ids_tab[1]);
+        break;
+
+        case 'port3':
+            create_ele(ids_tab[2]);
+        break;
+
+        case 'port4':
+            create_ele(ids_tab[3]);
+        break;
+
+    }
+}
+
+// function to create img ele for the modal
+function create_ele(t) {
+    var ele = document.getElementById('addPicture');
+    var img = document.createElement('img');
+    img.src = t;
+    img.id = "modl-img";
+    ele.appendChild(img);
+    document.getElementById("modl-img").classList.add('img-fluid')
+}
+
+// function to remove the img after closing the modal
+function deletefunc(){
+    var rem = document.getElementById('modl-img');
+    rem.remove();
+}
+// end of portfolio images modal
+
